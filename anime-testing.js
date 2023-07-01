e489dda16fbabae9ac6d7a568843ec6250c1b3fc
Avery <averycaldwell7@gmail.com>
2023-07-01 16:14:08 -0700

    adding transitions with elements

---
+++ anime-testing.js
@@ -0,0 +1,80 @@
+let animation = anime({
+    targets: '.letter',
+    opacity: 1,
+    translateY: 50,
+    rotate: {
+        value: 360,
+        duration: 2000,
+        easing: 'easeInExpo',
+    },
+    scale: anime.stagger([0.7, 1], { from: 'center' }),
+    delay: anime.stagger(100, { start: 1000 }),
+    translateX: [-10, 30],
+});
+
+let homeImage = document.querySelector('.homeImage');
+// homeImage.style.transform = 'translateY(0)';
+let animateY = anime({
+    targets: '.homeImage',
+    translateY: ['180px', '666px'], // Adjust the value as needed
+    easing: 'easeInOutQuad',
+    autoplay: false,
+});
+document.querySelector('.play-down').onclick = animateY.restart;
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to play the animation when the element is in the viewport
+function playAnimationOnScroll() {
+    let aboutHeader = document.querySelector('.aboutHeader');
+    let timeline = anime.timeline({ autoplay: false, loop: false }).add({
+        targets: '.aboutHeader ',
+        opacity: [0, 1],
+        translateY: ['180px', '480px'],
+        easing: 'easeInOutQuad',
+        duration: 1400,
+        delay: (el, i) => 50 * (i + 1),
+        endDelay: 500,
+    });
+
+    if (isInViewport(aboutHeader)) {
+        aboutHeader.innerHTML = aboutHeader.textContent.replace(
+            /\S/g,
+            "<span style='display: inline-block'>$&</span>"
+        );
+        timeline.play();
+        window.removeEventListener('scroll', playAnimationOnScroll);
+    }
+}
+
+// Function to play the animation on button click
+function playAnimation() {
+    let aboutHeader = document.querySelector('.aboutHeader');
+    aboutHeader.innerHTML = aboutHeader.textContent.replace(
+        /\S/g,
+        "<span style='display: inline-block'>$&</span>"
+    );
+
+    let timeline = anime.timeline({ autoplay: false, loop: false }).add({
+        targets: '.aboutHeader ',
+        opacity: [0, 1],
+        translateY: ['180px', '480px'],
+        easing: 'easeInOutQuad',
+        duration: 1400,
+        delay: (el, i) => 50 * (i + 1),
+        endDelay: 500,
+    });
+
+    timeline.play();
+}
--- index.js
+++ index.js
@@ -1,80 +1,49 @@
-let animation = anime({
-    targets: '.letter',
-    opacity: 1,
-    translateY: 50,
-    rotate: {
-        value: 360,
-        duration: 2000,
-        easing: 'easeInExpo',
-    },
-    scale: anime.stagger([0.7, 1], { from: 'center' }),
-    delay: anime.stagger(100, { start: 1000 }),
-    translateX: [-10, 30],
-});
+//array of pages
+const pages = ['Landing', 'About', 'Edu'];
+// state
+let currentPage = 'Landing';
 
-let homeImage = document.querySelector('.homeImage');
-// homeImage.style.transform = 'translateY(0)';
-let animateY = anime({
-    targets: '.homeImage',
-    translateY: ['180px', '666px'], // Adjust the value as needed
-    easing: 'easeInOutQuad',
-    autoplay: false,
-});
-document.querySelector('.play-down').onclick = animateY.restart;
+// sets the active page shown
+const setActivePage = (page) => {
+    const appContainer = document.getElementsByClassName('appContainer')[0];
+    const pageIndex = pages.indexOf(page);
+    const currentPageIndex = pages.indexOf(currentPage);
 
-// Function to check if an element is in the viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <=
-            (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+    if (page !== currentPage) {
+        const pageDiff =
+            pageIndex < currentPageIndex
+                ? Math.abs(currentPageIndex - pageIndex)
+                : Math.abs(pageIndex - currentPageIndex);
+        console.log(pageDiff);
+        const calculatedVH = pageIndex * 100;
+        // FIXME: needs to references existing position in array to dictate ease
+        appContainer.style.transition = `all ${pageDiff * 1.5}s ease`;
 
-// Function to play the animation when the element is in the viewport
-function playAnimationOnScroll() {
-    let aboutHeader = document.querySelector('.aboutHeader');
-    let timeline = anime.timeline({ autoplay: false, loop: false }).add({
-        targets: '.aboutHeader ',
-        opacity: [0, 1],
-        translateY: ['180px', '480px'],
-        easing: 'easeInOutQuad',
-        duration: 1400,
-        delay: (el, i) => 50 * (i + 1),
-        endDelay: 500,
-    });
-
-    if (isInViewport(aboutHeader)) {
-        aboutHeader.innerHTML = aboutHeader.textContent.replace(
-            /\S/g,
-            "<span style='display: inline-block'>$&</span>"
-        );
-        timeline.play();
-        window.removeEventListener('scroll', playAnimationOnScroll);
+        const logoSliderImage =
+            document.getElementsByClassName('logoSliderImage')[0];
+        const logoSliderCircle =
+            document.getElementsByClassName('logoSliderCircle')[0];
+        //     transition: all 1.5s ease;
+        // changing top prop value to move the image
+        if (pageIndex === 0) {
+            setTimeout(function () {
+                logoSliderImage.style.top = `15px`;
+            }, 1200);
+            logoSliderCircle.style.opacity = '0';
+        } else {
+            logoSliderImage.style.top = `calc(100% - 335px)`;
+            setTimeout(function () {
+                logoSliderCircle.style.opacity = '1';
+            }, 1000);
+        }
+        appContainer.style.top = `calc(0% - ${calculatedVH}vh)`;
+        currentPage = page;
     }
-}
+};
+// setActivePage();
 
-// Function to play the animation on button click
-function playAnimation() {
-    let aboutHeader = document.querySelector('.aboutHeader');
-    aboutHeader.innerHTML = aboutHeader.textContent.replace(
-        /\S/g,
-        "<span style='display: inline-block'>$&</span>"
-    );
+// for each page, we need to know the diff between the current page and all other pages {FIRST}
+// need to create an object that houses the diff from the 1st page
+// need to create a way of knowing which pages are between the current page and target page
 
-    let timeline = anime.timeline({ autoplay: false, loop: false }).add({
-        targets: '.aboutHeader ',
-        opacity: [0, 1],
-        translateY: ['180px', '480px'],
-        easing: 'easeInOutQuad',
-        duration: 1400,
-        delay: (el, i) => 50 * (i + 1),
-        endDelay: 500,
-    });
 
-    timeline.play();
-}
