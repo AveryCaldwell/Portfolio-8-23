3da928a88d9a4026fb963d3f33891f8ba9deb21d
Avery <averycaldwell7@gmail.com>
2023-07-13 17:11:26 -0700

    added edu slides

---
+++ data.js
@@ -0,0 +1,49 @@
+//    <div class="eduContent ">
+//             <li class="eduList">
+//                 <em> Clinton, MS | 2012-2016 | GPA: 3.93/4.0</em>
+//             </li>
+//             <li class="eduList">
+//                 Headerman Scholar
+//             </li>
+//             <li class="eduList">
+//                 Women in Business, Member & Speaker
+//             </li>
+//             <li class="eduList">
+//                 Outstanding Marketing Award
+//             </li>
+//             <li class="eduList">
+//                 Sandra Parks Award
+//             </li>
+//             <li class="eduList">
+//                 Who's Who
+//             </li>
+//             <li class="eduList">
+//                 Mortar Board
+//             </li>
+//             <li class="eduList">
+//                 Delta Mu Delta, Epsilon Iota Delta, Omicron Delta Kappa
+//             </li>
+//         </div> -->
+//         <!-- <div class="eduContent ">
+//             <li class="eduList">
+//                 <em>Taipei, Taiwan</em>
+//             </li>
+//             <li class="eduList">
+//                 Certificate of Completion
+//             </li>
+//             <li class="eduList">
+//                 Summer 2015
+//             </li>
+//         </div> -->
+//         <!-- <div class="eduContent ">
+//                                 <li class="eduList">
+//                                     <em>Full Stack Developer Coding Bootcamp</em>
+//                                 </li>
+//                                 <li class="eduList">
+//                                     Certificate of Completion | 2022-2023
+//                                 </li>
+
+//                                 <li class="eduList">
+//                                     GPA: 98.27%/100%
+//                                 </li>
+//                             </div>
