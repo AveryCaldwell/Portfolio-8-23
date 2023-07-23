cf5fa5e2bc5eabdc4a01598208aa3c2b69f9ec88
Avery <averycaldwell7@gmail.com>
2023-07-23 15:55:01 -0700

    added ref cards

--- anime-testing.js
+++ anime-testing.js
@@ -430,3 +430,52 @@ function togglePlainText() {
         buttonContainer.style.top = '78vh'; // Reset the top position of the button container
     }
 }
+
+//=================== REFERENCE PAGE===================
+const limits = 15.0;
+
+document.querySelectorAll('.card').forEach((card) => {
+    card.addEventListener('mousemove', (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const x = e.clientX - rect.left; //x position within the element.
+        const y = e.clientY - rect.top; //y position within the element.
+        const offsetX = x / rect.width;
+        const offsetY = y / rect.height;
+
+        const rotateY = offsetX * (limits * 2) - limits;
+        const rotateX = offsetY * (limits * 2) - limits;
+
+        const shadowOffsetX = offsetX * 32 - 16;
+        const shadowOffsetY = offsetY * 32 - 16;
+
+        card.style.boxShadow = `${(1 / 6) * -shadowOffsetX}px ${
+            (1 / 6) * -shadowOffsetY
+        }px 3px rgba(0, 0, 0, 0.051),
+      ${(2 / 6) * -shadowOffsetX}px ${
+            (2 / 6) * -shadowOffsetY
+        }px 7.2px rgba(0, 0, 0, 0.073),
+      ${(3 / 6) * -shadowOffsetX}px ${
+            (3 / 6) * -shadowOffsetY
+        }px 13.6px rgba(0, 0, 0, 0.09),
+      ${(4 / 6) * -shadowOffsetX}px ${
+            (4 / 6) * -shadowOffsetY
+        }px 24.3px rgba(0, 0, 0, 0.107),
+      ${(5 / 6) * -shadowOffsetX}px ${
+            (5 / 6) * -shadowOffsetY
+        }px 45.5px rgba(0, 0, 0, 0.129),
+      ${-shadowOffsetX}px ${-shadowOffsetY}px 109px rgba(0, 0, 0, 0.18)`;
+        card.style.transform = `perspective(1000px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
+
+        const glarePos = rotateX + rotateY + 90;
+        const glare = card.querySelector('.glare');
+        glare.style.left = `${glarePos}%`;
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.boxShadow =
+            '0px 0px 3px rgba(0, 0, 0, 0.051), 0px 0px 7.2px rgba(0, 0, 0, 0.073), 0px 0px 13.6px rgba(0, 0, 0, 0.09), 0px 0px 24.3px rgba(0, 0, 0, 0.107), 0px 0px 45.5px rgba(0, 0, 0, 0.129), 0px 0px 109px rgba(0, 0, 0, 0.18)';
+        card.style.transform = 'scale(1.0)';
+        const glare = card.querySelector('.glare');
+        glare.style.left = '100%';
+    });
+});
